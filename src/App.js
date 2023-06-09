9ce1281bf1884aba07cc9809f646b6ee30ae84e7
raig1988 <raig.iglesias@gmail.com>
2023-06-09 13:24:16 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network, Utils } from 'alchemy-sdk';
+import { useState, useRef } from 'react';
+import Nav from './components/Nav';
+// import Block from '../src/mocks/block.json';
 
 import './App.css';
 
@@ -20,17 +22,129 @@ const settings = {
 const alchemy = new Alchemy(settings);
 
 function App() {
-  const [blockNumber, setBlockNumber] = useState();
+  const [currentBlockTransactions, setCurrentBlockTransactions] = useState()
+  const [estimateGas, setEstimateGas] = useState()
+  const [feeData, setFeeData] = useState()
+  const [dateTransact, setDateTransact] = useState("")
+  const [dateEstGas, setDateEstGas] = useState("")
+  const [address, setAddress] = useState("")
+  const [tokenBalances, setTokenBalances] = useState()
+  const [finalBalance, setFinalBalance] = useState()
+  const inputAddressRef = useRef(null)
+  const gasCalc = (((parseInt(currentBlockTransactions?.gasUsed._hex) / 15000000) - 1) * 100).toFixed(0);
 
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+  const handleClick = async () => {
+    const block = await alchemy.core.getBlockNumber()
+    setCurrentBlockTransactions(await alchemy.core.getBlockWithTransactions(block))
+    setDateTransact(new Date())
+  }
+
+  const runEstimateGas = async () => {
+    const response = await alchemy.core.getGasPrice()
+    setEstimateGas(response);
+    const feeData = await alchemy.core.getFeeData()
+    setFeeData(feeData)
+    setDateEstGas(new Date())
+  }
+
+  const getTokenBalances = async () => {
+    const balances = await alchemy.core.getTokenBalances(address);
+    setTokenBalances(balances)
+    // logic to filter balances
+    if (tokenBalances) {
+      const arrayOfBalances = []
+      const nonZeroBalances = tokenBalances.tokenBalances.filter((token) => {
+          return token.tokenBalance !== "0x0000000000000000000000000000000000000000000000000000000000000000";
+      }) 
+      for (let token of nonZeroBalances) {
+        let balance = token.tokenBalance;
+        const metadata = await alchemy.core.getTokenMetadata(token.contractAddress);
+        balance = balance / Math.pow(10, metadata.decimals)
+        balance = balance.toFixed(2);
+        console.log(`${metadata.name} : ${balance} ${metadata.symbol}`)
+        arrayOfBalances.push(
+          `${metadata.name} : ${balance} ${metadata.symbol}`
+        )
+      }
+      setFinalBalance(arrayOfBalances)
+      console.log(finalBalance)
     }
+  }
 
-    getBlockNumber();
-  });
+  return (
+    <>
+      <Nav />
+      <div className="grid grid-cols-2">
+        <div className="flex flex-col mx-10 space-y-2">
+          <button className='bg-black text-white' type='submit' onClick={handleClick}>New Block</button>
+          {
+            currentBlockTransactions && dateTransact
+            ?
+            <>
+                <h2>Block Info</h2>
+                <p>Block Number: {currentBlockTransactions.number}</p>
+                <p>Hash: {currentBlockTransactions.hash}</p>
+                <p>Timestamp: {currentBlockTransactions.timestamp}</p>
+                <p>Gas limit: {parseInt(currentBlockTransactions.gasLimit._hex)}</p>
+                <p>Gas used: {parseInt(currentBlockTransactions.gasUsed._hex)}</p>
+                <p>Gas target: 15000000</p>
+                <p>Gas tendency: {`${gasCalc}%`}</p>
+                {
+                  gasCalc > 0 
+                  ? <p className="text-bold">Gas price will increase next block</p>
+                  : <p className="text-bold">Gas price will decrease next block</p>
+                }
+                <p>Number of transactions: {currentBlockTransactions.transactions.length}</p>
+                <p>Data from: {dateTransact.toString()}</p>
+              </>
+              : null
+          }
+        </div>
+        <div className="flex flex-col mx-10 space-y-2">
+          <button className='bg-black text-white' type='submit' onClick={runEstimateGas}>Estimate Gas</button>
+          {
+            estimateGas && feeData && dateEstGas
+            ? 
+              <>
+               <p>Estimate gas for a transaction:</p>
+                <p>Gas Price:</p>
+                <p>Wei: {(parseInt(estimateGas._hex))}</p>
+                <p>Gwei: {Utils.formatUnits((parseInt(estimateGas._hex)), 'gwei')}</p>
+                <p>Ether: {Utils.formatUnits((parseInt(estimateGas._hex)), 'ether')}</p>
+                <p>Fee data:</p>
+                <p>Max fee per gas in gwei: {Utils.formatUnits((parseInt(feeData.maxFeePerGas._hex)), 'gwei')}</p>
+                <p>Max priority fee in gwei: {Utils.formatUnits((parseInt(feeData.maxPriorityFeePerGas._hex)), 'gwei')}</p>
+                <p>Data from: {dateEstGas.toString()}</p>
+              </>
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+            : null
+          }
+        </div>
+      </div>
+      <div className="flex flex-col m-10 space-y-2">
+        <p>Token balances by address</p>
+        <label>Enter your Eth address</label>
+        <input ref={inputAddressRef} className="border-2 border-black" type="text" value={address} onChange={((e) => setAddress(e.target.value))} />
+        <button className='bg-black text-white w-25' type="submit" onClick={getTokenBalances}>Get token balances</button>
+        {
+          finalBalance
+          ? 
+            <div className="flex flex-col justify-center text-center">
+              {
+                finalBalance.map(item => {
+                  return (
+                    <p>{item.toString()}</p>
+                  )
+                })
+              }
+            </div>
+          : null
+        }
+      </div>
+    </>
+  )
 }
 
 export default App;
+
+          
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+export default function Nav() {
+    return (
+        <nav className="flex justify-center items-center h-20">
+            <h1 className="text-xl font-bold">CryptoExplorer</h1>
+        </nav>
+    )
+}
